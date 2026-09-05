117c76fa6567974e164bd95d0b64c62d09e062de
Iheb Mrabet <ihabmrabet12@gmail.com>
2026-09-05 12:15:02 +0100

    fix(infra): harden Academy cluster bootstrap

--- infra/scripts/remote-bootstrap.sh
+++ infra/scripts/remote-bootstrap.sh
@@ -5,7 +5,12 @@ set -Eeuo pipefail
 ROBOTEC_SSH_USER="${ROBOTEC_SSH_USER:-ubuntu}"
 ROBOTEC_SSH_KEY="${ROBOTEC_SSH_KEY:-}"
 
-ssh_args=(-o BatchMode=yes -o StrictHostKeyChecking=accept-new)
+ssh_args=(-o BatchMode=yes)
+if ssh -G -o StrictHostKeyChecking=accept-new localhost >/dev/null 2>&1; then
+  ssh_args+=(-o StrictHostKeyChecking=accept-new)
+else
+  ssh_args+=(-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null)
+fi
 [[ -n "${ROBOTEC_SSH_KEY}" ]] && ssh_args+=(-i "${ROBOTEC_SSH_KEY}")
 ssh "${ssh_args[@]}" "${ROBOTEC_SSH_USER}@${ROBOTEC_HOST}" \
   'sudo cloud-init status --wait && sudo /usr/local/sbin/robotek-bootstrap-k3s && sudo /usr/local/sbin/robotek-bootstrap-argocd'
